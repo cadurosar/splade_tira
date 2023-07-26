f93008a7a72a142ba381a663b8898f00b1c3a7f8
Carlos <carlos.lassance@naverlabs.com>
2023-07-26 05:46:25 +0200

    add parameters

--- run_retrieval.py
+++ run_retrieval.py
@@ -12,38 +12,48 @@ from tqdm import tqdm
 
 import pyt_splade
 
+def parse_args():
+    parser = argparse.ArgumentParser(prog='Retrieve with SPLADE via PyterrierPisa.')
 
-ensure_pyterrier_is_loaded()
-input_directory, output_directory = get_input_directory_and_output_directory('./splade_tira/sample-input-full-rank/')
+    parser.add_argument('--input', required=True)
+    parser.add_argument('--output', required=True)
 
-
-ensure_pyterrier_is_loaded()
-input_directory, output_directory = get_input_directory_and_output_directory('./sample-input-full-rank')
-
-print('Step 2: Load the data.')
-
-queries = pt.io.read_topics(input_directory + '/queries.xml', format='trecxml')
-
-documents = [json.loads(i) for i in open(input_directory + '/documents.jsonl', 'r')]
-
-print('Step 3: Create the Index.')
-splade = pyt_splade.SpladeFactory()
-iter_indexer = pt.IterDictIndexer("./index", pretokenised=True, meta={'docno' : 100})
-
-from pyterrier_pisa import PisaIndex
-index = PisaIndex('./index', stemmer='none')
-
-# indexing
-idx_pipeline = splade.indexing() >> index.toks_indexer()
-
-index_ref = idx_pipeline.index(documents, batch_size=2)
+    return parser.parse_args()
 
 
-print('Step 4: Create Run.')
-splade_retr = splade.query() >> index.quantized()
-run = splade_retr(queries)
+def main(args):
+    
+    ensure_pyterrier_is_loaded()
+    input_directory, output_directory = get_input_directory_and_output_directory(args.input)
+    
+    print('Step 2: Load the data.')
+    
+    queries = pt.io.read_topics(input_directory + '/queries.xml', format='trecxml')
+    
+    documents = [json.loads(i) for i in open(input_directory + '/documents.jsonl', 'r')]
+    
+    print('Step 3: Create the Index.')
+    splade = pyt_splade.SpladeFactory()
+    iter_indexer = pt.IterDictIndexer("./index", pretokenised=True, meta={'docno' : 100})
+    
+    from pyterrier_pisa import PisaIndex
+    index = PisaIndex('./index', stemmer='none')
+    
+    # indexing
+    idx_pipeline = splade.indexing() >> index.toks_indexer()
+    
+    index_ref = idx_pipeline.index(documents, batch_size=2)
+    
+    
+    print('Step 4: Create Run.')
+    splade_retr = splade.query() >> index.quantized()
+    run = splade_retr(queries)
+    
+    
+    print('Step 5: Persist Run.')
+    
+    persist_and_normalize_run(run, output_file=args.output, system_name='SPLADE++-CoCondenser-EnsembleDistil', depth=1000)
 
-
-print('Step 5: Persist Run.')
-
-persist_and_normalize_run(run, output_file=output_directory, system_name='SPLADE++-CoCondenser-EnsembleDistil', depth=1000)
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
